8e27e15ce5d95be2f2d6459a89b410aec16de7a5
Evsyukov Denis Anatolyevich <d.evsyukov@tinkoff.ru>
2019-06-17 10:28:56 +0300

    Added keymap for russian osx keyboard

---
+++ keys.el
@@ -0,0 +1,151 @@
+;;; cyrillic.el --- Quail package for inputting Cyrillic characters
+
+;; Copyright (C) 1997-1998, 2001-2018 Free Software Foundation, Inc.
+;; Copyright (C) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
+;;   2006, 2007, 2008, 2009, 2010, 2011
+;;   National Institute of Advanced Industrial Science and Technology (AIST)
+;;   Registration Number H14PRO021
+
+;; Author: TAKAHASHI Naoto <ntakahas@m17n.org>
+;; Keywords: multilingual, input method, Cyrillic, i18n
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; These methods use a mixture of 8859-5 and Unicode.  Quail, used
+;; with ucs-tables provides support for translating on the fly to
+;; what's appropriate for aa buffer's file coding system, so the
+;; encoding shouldn't matter too much provided it supports the
+;; necessary characters.
+
+;;; Code:
+
+(require 'quail)
+
+;; See comment above.  This is the variant `winkeys' from `ru' in XKB.
+(quail-define-package
+ "russian-mac" "Russian" "RU" nil
+ "ЙЦУКЕН Russian mac layout"
+ nil t t t t nil nil nil nil nil t)
+
+;;  1! 2" 3№ 4% 5: 6, 7. 8; 9( 0) -_ =+ \/ ёЁ
+;;   Й  Ц  У  К  Е  Н  Г  Ш  Щ  З  Х  Ъ
+;;    Ф  Ы  В  А  П  Р  О  Л  Д  Ж  Э
+;;     Я  Ч  С  М  И  Т  Ь  Б  Ю  /?
+
+(quail-define-rules
+ ("1" ?1)
+ ("2" ?2)
+ ("3" ?3)
+ ("4" ?4)
+ ("5" ?5)
+ ("6" ?6)
+ ("7" ?7)
+ ("8" ?8)
+ ("9" ?9)
+ ("0" ?0)
+ ("-" ?-)
+ ("=" ?=)
+ ("|" ?/)
+ ("`" ?ё)
+ ("q" ?й)
+ ("w" ?ц)
+ ("e" ?у)
+ ("r" ?к)
+ ("t" ?е)
+ ("y" ?н)
+ ("u" ?г)
+ ("i" ?ш)
+ ("o" ?щ)
+ ("p" ?з)
+ ("[" ?х)
+ ("]" ?ъ)
+ ("a" ?ф)
+ ("s" ?ы)
+ ("d" ?в)
+ ("f" ?а)
+ ("g" ?п)
+ ("h" ?р)
+ ("j" ?о)
+ ("k" ?л)
+ ("l" ?д)
+ (";" ?ж)
+ ("'" ?э)
+ ("\\" ?\\)
+ ("z" ?я)
+ ("x" ?ч)
+ ("c" ?с)
+ ("v" ?м)
+ ("b" ?и)
+ ("n" ?т)
+ ("m" ?ь)
+ ("," ?б)
+ ("." ?ю)
+ ("/" ?/)
+ ("!" ?!)
+ ("@" ?\")
+ ("#" ?№)
+ ("$" ?%)
+ ("%" ?:)
+ ("^" ?,)
+ ("&" ?.)
+ ("*" ?\;)
+ ("(" ?\()
+ (")" ?\))
+ ("_" ?_)
+ ("+" ?+)
+ ("~" ?Ё)
+ ("Q" ?Й)
+ ("W" ?Ц)
+ ("E" ?У)
+ ("R" ?К)
+ ("T" ?Е)
+ ("Y" ?Н)
+ ("U" ?Г)
+ ("I" ?Ш)
+ ("O" ?Щ)
+ ("P" ?З)
+ ("{" ?Х)
+ ("}" ?Ъ)
+ ("A" ?Ф)
+ ("S" ?Ы)
+ ("D" ?В)
+ ("F" ?А)
+ ("G" ?П)
+ ("H" ?Р)
+ ("J" ?О)
+ ("K" ?Л)
+ ("L" ?Д)
+ (":" ?Ж)
+ ("\"" ?Э)
+ ("|" ?|)
+ ("Z" ?Я)
+ ("X" ?Ч)
+ ("C" ?С)
+ ("V" ?М)
+ ("B" ?И)
+ ("N" ?Т)
+ ("M" ?Ь)
+ ("<" ?Б)
+ (">" ?Ю)
+ ("?" ?/))
+
+;; Local Variables:
+;; coding: utf-8
+;; End:
+
+;;; cyrillic.el ends here
